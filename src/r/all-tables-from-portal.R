103e9f08d2ee3028d83bcfd0fc8e88dcfcec5636
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-05-08 08:04:55 +0100

    switch to jsonrpc

--- src/r/all-tables-from-portal.R
+++ src/r/all-tables-from-portal.R
@@ -14,11 +14,11 @@ fetch_dataset <- function(matrix,
   repeat {
     result <- tryCatch(
       {
+        
         url <- paste0(
-          "https://ws-data.nisra.gov.uk/public/api.restful/",
-          "PxStat.Data.Cube_API.ReadDataset/",
+          "https://ws-data.nisra.gov.uk/public/api.jsonrpc?data=%7B%22jsonrpc%22:%222.0%22,%22method%22:%22PxStat.Data.Cube_API.ReadDataset%22,%22params%22:%7B%22class%22:%22query%22,%22id%22:%5B%5D,%22dimension%22:%7B%7D,%22extension%22:%7B%22pivot%22:null,%22codes%22:false,%22language%22:%7B%22code%22:%22en%22%7D,%22format%22:%7B%22type%22:%22JSON-stat%22,%22version%22:%222.0%22%7D,%22matrix%22:%22",
           matrix,
-          "/JSON-stat/2.0/en?apiKey=",
+          "%22%7D,%22version%22:%222.0%22%7D%7D&apiKey=",
           api_key
         )
 
@@ -128,7 +128,7 @@ for (i in seq_along(data_portal$label)) {
 
   matrix <- data_portal$extension$matrix[i]
 
-  json_data <- fetch_dataset(matrix, api_key, data_portal$label[i])
+  json_data <- fetch_dataset(matrix, api_key, data_portal$label[i])$result
 
   subject <- json_data$extension$subject$value
   product_code <- json_data$extension$product$code
