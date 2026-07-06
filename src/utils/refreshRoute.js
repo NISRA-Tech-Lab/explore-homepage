804107159f2e7a827e834d7fc2a6e3e86110d047
Mireland998 <Mathew.Ireland@finance-ni.gov.uk>
2026-07-06 16:59:11 +0100

    Remove unused import statements

--- src/utils/refreshRoute.js
+++ src/utils/refreshRoute.js
@@ -1,10 +1,9 @@
 import { clearElements } from "./clearElements.js";
 import { createMenus } from "./createMenus.js";
-import { map_card, chart_card } from "./elements.js";
 
 export async function refreshRoute() {
 
     await clearElements();
-    
+
     await createMenus();
 }
