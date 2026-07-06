dada2394000037cd4f57279c8cfede581f263829
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-07-06 14:37:39 +0100

    data portal preview fn

--- src/utils/plotMap.js
+++ src/utils/plotMap.js
@@ -8,12 +8,15 @@ import { themes_menu, map_container, stats_menu,
          table_preview, metadata_text, search, geo_menu,
          SIDEBAR_OPEN_KEY, map_card, chart_card, dp_link, 
          chart_updated, nav_product, nav_subject, nav_theme,
-         table_title, map_updated, map_title, headline_stat_label,
-         additional_tables, table_updated, stat_info_text } from "./elements.js";     
+         table_title, map_updated, map_title, 
+         headline_stat_label, additional_tables, table_updated, 
+         stat_info_text } from "./elements.js";     
 import { downloadButton } from "./download-button.js";
 import { buildCharts } from "./buildCharts.js";
 import { buildTables } from "./buildTables.js";
-import { addOtherMenus, id_vars, other_selections, other_headline, other_vars, subtitle_text } from "./addOtherMenus.js";
+import { addOtherMenus, id_vars, other_selections, other_headline,
+         other_vars, subtitle_text } from "./addOtherMenus.js";
+import { dataPortalPreview } from "./dataPortalPreview.js";
 
 export let map;
 
@@ -40,14 +43,13 @@ export async function plotMap (tables, geog_type) {
             '},"extension":{"pivot":null,"codes":false,"language":{"code":"en"},"format":{"type":"JSON-stat","version":"2.0"},"matrix":"' +
             matrix + '"},"version":"2.0"}}');
 
-
     const response = await fetch(api_url);
     const {result} = await response.json();
 
     await buildTables(tables, matrix, statistic, geog_type, year, time_var, other_vars, other_selections, id_vars);
 
-    var stat_label = Object.values(result.dimension.STATISTIC.category.label)[0];
-    var unit = result.dimension.STATISTIC.category.unit[statistic].label;
+    const stat_label = Object.values(result.dimension.STATISTIC.category.label)[0];
+    const unit = result.dimension.STATISTIC.category.unit[statistic].label;
 
     let plot_ni = false;
 
@@ -408,109 +410,11 @@ export async function plotMap (tables, geog_type) {
 
         nav_theme.textContent = tables[geo_menu.value].theme;        
         nav_subject.textContent = tables[geo_menu.value].subject;    
-        nav_product.textContent = tables[geo_menu.value].product;   
-
+        nav_product.textContent = tables[geo_menu.value].product;
+        
         chart_updated.innerHTML = `Last updated: <strong>${result.updated.substr(8, 2)}/${result.updated.substr(5, 2)}/${result.updated.substr(0, 4)}</strong>. See this full dataset on <a href = "https://data.nisra.gov.uk/table/${matrix}" target = "_blank">NISRA Data Portal.</a>`;
 
-        let rows = tables[matrix].rows;
+        dataPortalPreview(tables, matrix, data, result, stat_label, geog_type, year, unit);       
 
-        dp_link.innerHTML = `Showing rows 1-${Math.min(data.length, 10)} of ${rows.toLocaleString("en-GB")}. See this full dataset on <a href = "https://data.nisra.gov.uk/table/${matrix}" target = "_blank">NISRA Data Portal</a> or download it in <a href = "https://ws-data.nisra.gov.uk/public/api.restful/PxStat.Data.Cube_API.ReadDataset/${matrix}/CSV/1.0/en">CSV format</a>.`
-
-         while (table_preview.firstChild) {
-            table_preview.removeChild(table_preview.firstChild)
-         }
-
-         let header_row = document.createElement("tr");
-
-         let headers = Object.keys(result.dimension);
-
-         for (let i = 0; i < headers.length; i ++) {
-            if (headers[i] != "NI") {
-                let th = document.createElement("th");
-                th.textContent = result.dimension[headers[i]].label;
-                header_row.appendChild(th);
-            }
-          
-
-         }
-
-         let unit_header = document.createElement("th");
-         let value_header = document.createElement("th");
-
-         unit_header.textContent = "Unit";
-         value_header.textContent = "Value";
-         value_header.classList.add("text-end");
-
-         header_row.appendChild(unit_header);
-         header_row.appendChild(value_header);
-
-         table_preview.appendChild(header_row);
-
-         for (let i = 0; i < Math.min(data.length, 10); i ++) {
-            let tr = document.createElement("tr");
-
-            let stat_cell = document.createElement("td");
-            stat_cell.textContent = stat_label;
-            tr.appendChild(stat_cell);
-
-            let year_cell = document.createElement("td");
-            if (["none", "NI"].includes(geog_type)) {
-                year_cell.textContent = time_series[i];
-            } else {
-                year_cell.textContent = year;
-            }
-            tr.appendChild(year_cell);
-
-            if (!["none", "NI"].includes(geog_type)) {
-                let geog_cell = document.createElement("td");
-                geog_cell.textContent = titleCase(Object.values(result.dimension[geog_type].category.label)[i]);
-                tr.appendChild(geog_cell);
-            }
-
-            for (let j = 0; j < other_vars.length; j ++) {
-                let other_cell = document.createElement("td");
-                other_cell.textContent = Object.values(result.dimension[other_vars[j]].category.label)[0];
-                tr.append(other_cell);
-            }
-
-            let unit_cell = document.createElement("td");
-            unit_cell.textContent = unit;
-            tr.appendChild(unit_cell);
-
-            let value_cell = document.createElement("td");
-            if (data[i] == null) {
-                value_cell.textContent = "..";
-            } else {
-                let decimals = result.dimension.STATISTIC.category.unit[stats_menu.value].decimals;
-                value_cell.textContent = data[i].toLocaleString("en-GB", {
-                    minimumFractionDigits: decimals,
-                    maximumFractionDigits: decimals
-                });
-            }
-            
-            value_cell.style = "text-align: right;";
-            tr.appendChild(value_cell);
-
-            table_preview.appendChild(tr);
-         }
-
-         let note_cleaned = result.note[0].replaceAll("\r", "<br>").replaceAll("[b]", "<strong>").replaceAll("[/b]", "</strong>").replaceAll("[i]", "<em>").replaceAll("[/i]", "</em>").replaceAll("[u]", "<u>").replaceAll("[/u]", "</u>");
-
-         // Convert [url=...]...[/url] into <a href="...">...</a>
-        note_cleaned = note_cleaned.replace(
-            /\[url=([a-zA-Z][a-zA-Z0-9+.-]*:[^\]]+)\](.*?)\[\/url\]/gi,
-            (match, url, text) => {
-                if (url.toLowerCase().startsWith("mailto:")) {
-                return `<a href="${url}">${text}</a>`;
-                } else {
-                return `<a href="${url}" target="_blank">${text}</a>`;
-                }
-            }
-        );
-
-
-
-         metadata_text.innerHTML = note_cleaned;   
-
-         downloadButton(matrix);
+        downloadButton(matrix);
 }
